6df51cc916d76a7465d1253344f4b326395ec29c
Flo569 <florianbothe5@gmail.com>
2026-02-02 17:57:35 +0100

    Bug fix -> wrong/old squares

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import settings
 from settings import Settings
 
 def main():
-    Settings.create_environment()
-    Settings.create_agent()
     settings.implement_layout()
     Settings.print_layout()
+    Settings.create_environment()
+    Settings.create_agent()
     Settings.world.train(Settings.episodes)
 
 if __name__ == "__main__":
